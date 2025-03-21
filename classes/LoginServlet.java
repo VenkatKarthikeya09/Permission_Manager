89c92f446aea526aea8c55d3b91a6decca8656b4
Venkat Karthikeya <125582718+VenkatKarthikeya09@users.noreply.github.com>
2025-03-21 10:56:00 +0530

    Create LoginServlet.java

---
+++ classes/LoginServlet.java
@@ -0,0 +1,43 @@
+//package com.permissionmanager;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        try {
+            Connection conn = DBConnection.getConnection();
+            String query = "SELECT * FROM users WHERE email = ? AND password = ?";
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, email);
+            pstmt.setString(2, password);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                HttpSession session = request.getSession();
+                session.setAttribute("user", email);
+                response.sendRedirect("welcome.html");
+            } else {
+                response.sendRedirect("index.html?error=invalid_credentials");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendRedirect("index.html?error=server_error");
+        }
+    }
+}
+
